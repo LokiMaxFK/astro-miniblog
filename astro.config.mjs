095e85339568b3c3256564ee02e546930e3a5f20
Jordan <jordanacrga@gmail.com>
2024-02-28 08:38:01 -0600

    Página terminada

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://alexcgdesign.github.io',
-    base: '/astro-miniblog',
+    site: 'https://lokimaxfk.github.io/',
+    base: '/astro-miniblog/',
 });
