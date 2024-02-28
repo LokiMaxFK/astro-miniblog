f5f256890f6f2ed8717c60b9fbb00a9a810b55bc
Jordan <jordanacrga@gmail.com>
2024-02-28 07:14:54 -0600

    Build de produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com"
+    site: 'https://alexcgdesign.github.io',
+    base: '/astro-miniblog',
 });
